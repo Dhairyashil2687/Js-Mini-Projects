bc38d99c80368bcfd163628fbab33e6d7001f42d
K Dhairyashil <dhairyakatle@gmail.com>
2026-09-15 00:55:25 +0530

    Added Notes App

---
+++ 4 Notes App/script.js
@@ -0,0 +1,34 @@
+const btn = document.querySelector('.btn');
+const container = document.querySelector('.app');
+
+function createNote(content = "") {
+    const note = document.createElement('textarea');
+    note.classList.add('note');
+    note.placeholder = "Enter Note..";
+    note.value = content;
+
+    note.addEventListener('dblclick', () => {
+        note.remove();
+        saveNotes();
+    });
+    note.addEventListener('input', saveNotes);
+
+    container.appendChild(note);
+
+}
+
+function saveNotes() {
+    const notes = document.querySelectorAll('.note');
+    const data = [];
+    notes.forEach(note => data.push(note.value));
+    localStorage.setItem('my-notes', JSON.stringify(data));
+}
+
+function loadNotes() {
+    const saved = JSON.parse(localStorage.getItem('my-notes') || "[]");
+    saved.forEach(noteText => createNote(noteText));
+}
+
+btn.addEventListener('click', () => createNote());
+
+loadNotes();
